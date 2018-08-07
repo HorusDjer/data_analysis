0ec6cec07d63d2496af8a1068025f292fa6e301b
HorusDjer <eobineme@gmail.com>
2018-08-07 10:59:40 -0400

    finished data check script

--- data_check.py/max_date.py
+++ data_check.py/max_date.py
@@ -2,38 +2,47 @@ from datetime import datetime
 import time
 import os
 
-# written by Ejike in python 2.7
+# written in python 2.7
+
+print "Starting lookup..."
 
 # note: catalist updates file name to reflect date modified.
 todays_date = datetime.now().strftime('%m%d%y')
-
-
 file_path = '/data/vansync/ContactsContacts_VF_{}{}'.format(
     todays_date, '.txt')
+
 try:
     with open(file_path, "r") as file_object:
         contents = file_object.readlines()
+        all_canvassed_dates = []
+        for line in contents[-200000:-1]:
+            if line != contents[0]: # use in case we need to parse out whole file.
+                record = line.split()
+                canvassed_date = record[7] + " " + record[8]
+                datetime_object = datetime.strptime(canvassed_date, '%Y-%m-%d %H:%M:%S')
+                all_canvassed_dates.append(datetime_object)
 except IOError:
     msg = "The file path used does not exist"
     print msg
 
 try:
     date_modified = time.ctime(os.path.getmtime(file_path))
-    last_record = contents[-1].split("\t")
-    date_canvassed = last_record[6]
+    last_record = contents[-1].split("\t") # assumes last row contains most recent date created.
     date_created = last_record[5]
+    
     print contents[0]
-
-    print "searching for last record"
+    print "searching"
     time.sleep(1)
     print "..."
     time.sleep(1.5)
     # print contents[-1]
     print "..."
     time.sleep(2)
-    print "The last date canvassed in contacts_contacts_vf is:", date_canvassed
+    print "The last date canvassed in contacts_contacts_vf is:", max(all_canvassed_dates)
     print "The last date created in contacts_contacts_vf is:  ", date_created
     print "\nThis file was last modified", date_modified
+    # print "The length of the list: ", len(all_canvassed_dates)
+    
     print "\nLookup complete"
 except NameError:
     pass
--- data_check.py/max_date_test.py
+++ data_check.py/max_date_test.py
@@ -6,19 +6,53 @@ import os
 # note: file name reflects date modified
 todays_date = datetime.now().strftime('%m%d%y')
 
-file_path = '/data/vansync/ContactsContacts_VF_{}{}'.format(
-    todays_date, '.txt')
-# file_path = '/data/vansync/ContactsContacts_VF_080518.txt'
+""" file_path = '/data/vansync/ContactsContacts_VF_{}{}'.format(
+    todays_date, '.txt') """
+
 file_path = "/Users/ejikeobineme/Documents/data_analysis/data_check.py/contacts_contact.txt"
 date_modified = time.ctime(os.path.getmtime(file_path))
 
-with open(file_path, "r") as file_object:
-    contents = file_object.readlines()
+""" try:
+    with open(file_path, "r") as file_object:
+        contents = file_object.readlines()
+        all_canvassed_dates = []
+        for line in contents:
+            if line != contents[0]:
+                record = line.split()
+                canvassed_date = record[7] + " " + record[8]
+                datetime_object = datetime.strptime(canvassed_date, '%Y-%m-%d %H:%M:%S')
+                all_canvassed_dates.append(datetime_object) """
 
-# put last string into a list to be evaluated.
-last_record = contents[-1].split("\t")
-date_canvassed = last_record[6]
-print(last_record)
-print(contents[-1])
-print("The last canvassed date recorded in contacts_contact_vf is:", date_canvassed)
-print("\nThis file was last modified", date_modified)
+try:
+    with open(file_path, "r") as file_object:
+        contents = file_object.readlines()
+        for line in contents:
+            if line != contents[0]:
+                
+                all_canvassed_dates = list(map(datetime.strptime(canvassed_date, '%Y-%m-%d%H:%M:%S'), line.split()[7:9]))             
+
+except IOError:
+    msg = "The file path used does not exist"
+    print(msg)
+
+try:
+    date_modified = time.ctime(os.path.getmtime(file_path))
+    last_record = contents[-1].split("\t")
+    date_canvassed = last_record[6]
+    date_created = last_record[5]
+    print(contents[0])
+
+    print("searching for last record")
+    # time.sleep(1)
+    print("...")
+    # time.sleep(1.5)
+    # print contents[-1]
+    print("...")
+    # time.sleep(2)
+    print("The last date canvassed in contacts_contacts_vf is:", max(all_canvassed_dates))
+    print("The last date created in contacts_contacts_vf is:  ", date_created)
+    print("\nThis file was last modified", date_modified)
+
+    print("\nLookup complete")
+except NameError:
+    pass
